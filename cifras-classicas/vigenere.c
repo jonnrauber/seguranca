b3f0165c6dc5b650f3dc27bd3da9332bfa2ceead
jonnrauber <natanrauber97@gmail.com>
2018-09-13 20:22:51 -0300

    descriptografia Vigenère

--- cifras-classicas/vigenere.c
+++ cifras-classicas/vigenere.c
@@ -40,7 +40,7 @@ int is_lower_case(char c) {
  * texto e produzir a criptografia.
  * */
 char change_character(char c_text, char c_key, int* key_pos) {
-  if (is_lower_case(c_text) == -1)
+    if (is_lower_case(c_text) == -1)
         return c_text;
     *key_pos += 1;
     if (is_lower_case(c_text) && is_lower_case(c_key))
@@ -51,23 +51,54 @@ char change_character(char c_text, char c_key, int* key_pos) {
         return ((c_text - 'a') + (c_key - 'A')) % 26 + 'a';
     if (! is_lower_case(c_text) && is_lower_case(c_key))
         return ((c_text - 'A') + (c_key - 'a')) % 26 + 'A';
+    return ' ';
+}
+
+/*
+ * Usa a "tabula recta", ou quadrado de Vigenere para descriptografar o
+ * caractere passado e retornar ao caractere original do texto claro.
+ * */
+char decrypt_character(char c_enc, char c_key, int* key_pos) {
+    if (is_lower_case(c_enc) == -1)
+        return c_enc;
+    *key_pos += 1;
+    if (is_lower_case(c_enc) && is_lower_case(c_key))
+        return ((c_enc - 'a') - (c_key - 'a') + 26) % 26 + 'a';
+    if (! is_lower_case(c_enc) && ! is_lower_case(c_key))
+        return ((c_enc - 'A') - (c_key - 'A') + 26) % 26 + 'A';
+    if (is_lower_case(c_enc) && ! is_lower_case(c_key))
+        return ((c_enc - 'a') - (c_key - 'A') + 26) % 26 + 'a';
+    if (! is_lower_case(c_enc) && is_lower_case(c_key))
+        return ((c_enc - 'A') - (c_key - 'a') + 26) % 26 + 'A';
+    return ' ';
 }
 
 /*
  * Criptografa com a cifra de Vigenere.
  * Recebe um texto claro e uma chave e retorna o texto criptografado.
  * */
-char* vigenere(char* text, char* key) {
-    char* encrypted = NULL;
+char* vigenere_encrypt(char* text, char* key) {
     int i, j;
+    char* encrypted = (char*)malloc(sizeof(char)*strlen(text));
 
-    for (i = 0, j = 0; i < strlen(text); i++) {
-        encrypted = (char *)realloc(encrypted, sizeof(char)*(i+1));
+    for (i = 0, j = 0; i < strlen(text); i++)
         encrypted[i] = change_character(text[i], key[j % strlen(key)], &j);
-    }
-    encrypted = (char *)realloc(encrypted, sizeof(char)*(i+1));
     encrypted[i] = '\0';
     return encrypted;
+}
+
+/*
+ * Recebe um texto criptografado e a respectiva chave de criptografia.
+ * Retorna o texto claro original.
+ * */
+char* vigenere_decrypt(char* text, char* key) {
+    int i, j;
+    char* decrypted = (char*)malloc(sizeof(char)*strlen(text));
+
+    for (i = 0, j = 0; i < strlen(text); i++)
+        decrypted[i] = decrypt_character(text[i], key[j % strlen(key)], &j);
+    decrypted[i] = '\0';
+    return decrypted;
 }
 
 /*
@@ -94,9 +125,15 @@ int main(void) {
     read(key);
 
     //criptografa com a cifra de vigenere
-    text = vigenere(cleartext, key);
+    text = vigenere_encrypt(cleartext, key);
 
     printf("-- Texto criptografado em cifra de Vigenere: --\n");
+    printf("%s\n", text);
+
+    //descriptografa novamente
+    text = vigenere_decrypt(text, key);
+
+    printf("\nTexto novamente descriptografado: \n");
     printf("%s\n", text);
 
     free(text);
